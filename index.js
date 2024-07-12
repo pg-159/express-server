a2b13f4c9417d596d04b16b10dea09c0b9b9fc10
pg-159 <pratap.gadhvi159@gmail.com>
2024-07-12 07:20:16 +0000

    endpoint-2: membership-discount added

--- index.js
+++ index.js
@@ -9,12 +9,33 @@ let taxRate = 5; // 5%
 let discountPercentage = 10; // 10%
 let loyaltyRate = 2; // 2 points per $1
 
+// function to calculate cart total
+function calculateCartTotal (newItemPrice, cartTotal){
+  cartTotal = cartTotal + newItemPrice;
+  return cartTotal;
+}
+
 app.get("/cart-total", (req, res) => {
   let newItemPrice = parseFloat(req.query.newItemPrice);
   let cartTotal = parseFloat(req.query.cartTotal);
-  cartTotal = cartTotal + newItemPrice;
   
   res.send(cartTotal.toString());
+})
+
+function applyDiscount(cartTotal, isMember){
+
+  if (isMember === "true"){
+    cartTotal = cartTotal - (cartTotal * (discountPercentage / 100));
+    return cartTotal;
+  } else {
+    return cartTotal;
+  }
+}
+app.get("/membership-discount", (req, res) => {
+  let cartTotal = parseFloat(req.query.cartTotal);
+  let isMember = req.query.isMember;
+
+  res.send(applyDiscount(cartTotal, isMember).toString())
 })
 app.listen(port, () => {
   console.log("Server is running on http://localhost: ", port);
